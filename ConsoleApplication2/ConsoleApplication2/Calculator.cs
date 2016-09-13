3071c9a42a31b8b7229f8eab46e4c9f11058a18c
MashaOrlova <maria-orlova95@mail.ru>
2016-09-13 16:52:08 +0300

    add project

---
+++ ConsoleApplication2/ConsoleApplication2/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Calculator
+    {
+        public double calculation(double operant1, double operant2, int operations)
+        {
+                if (operations == 1)
+                {
+                    return operant1 + operant2;
+                }
+                else if (operations == 2)
+                {
+                    return operant1 - operant2;
+                }
+                else if (operations == 3)
+                {
+                    return operant1 * operant2;
+                }
+                else if (operations == 4)
+                {
+                    return operant1 / operant2;
+                }
+                else
+                     Console.WriteLine("Ошибка");
+            return 0;           
+        }
+    }
+}
