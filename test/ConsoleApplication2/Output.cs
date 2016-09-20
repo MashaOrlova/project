223d4820ccf165998c7fbee0d83cc2c38f2d9637
MashaOrlova <maria-orlova95@mail.ru>
2016-09-20 15:09:00 +0300

    Testing

---
+++ test/ConsoleApplication2/Output.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Output
+    {
+        public void vivod()
+        {
+            Calculator calcul = new Calculator();
+            int operations;
+            double operant1;
+            double operant2;
+            double answer;
+
+            Console.WriteLine("Hello! This is Kalculator");
+
+            Console.WriteLine("Введите первое число:");
+            operant1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите нужное действие: + (1), - (2), * (3), / (4)");
+            operations = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            operant2 = Convert.ToDouble(Console.ReadLine());
+                       
+            answer = calcul.calculation(operant1, operant2, operations);
+            Console.WriteLine("Результат вычисления: {0}", answer);           
+        }
+        
+    }
+}
---
+++ test/TestCalc/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleApplication2;
+
+namespace TestCalc
+{
+    public class Class1
+    {
+        [Test]
+        public void sum1()
+        {
+            Assert.AreEqual(7, new ConsoleApplication2.Calculator().calculation(5, 5, 1));
+        }
+        [Test]
+        public void sum2()
+        {
+            Assert.AreEqual(10, new ConsoleApplication2.Calculator().calculation(2, 8, 1));
+        }
+        [Test]
+        public void sum3()
+        {
+            Assert.AreEqual(1, new ConsoleApplication2.Calculator().calculation(2, 8, 1));
+        }
+        [Test]
+        public void razn1()
+        {
+            Assert.AreEqual(1, new ConsoleApplication2.Calculator().calculation(4, 3, 2));
+        }
+        [Test]
+        public void razn2()
+        {
+            Assert.AreEqual(5, new ConsoleApplication2.Calculator().calculation(10, 5, 2));
+        }
+        [Test]
+        public void razn3()
+        {
+            Assert.AreEqual(1, new ConsoleApplication2.Calculator().calculation(44, 23, 2));
+        }
+        [Test]
+        public void ymnoz1()
+        {
+            Assert.AreEqual(12, new ConsoleApplication2.Calculator().calculation(4, 3, 3));
+        }
+        [Test]
+        public void ymnoz2()
+        {
+            Assert.AreEqual(1, new ConsoleApplication2.Calculator().calculation(44, 33, 3));
+        }
+        [Test]
+        public void ymnoz3()
+        {
+            Assert.AreEqual(782, new ConsoleApplication2.Calculator().calculation(4, 38, 3));
+        }
+        [Test]
+        public void delen1()
+        {
+            Assert.AreEqual(12, new ConsoleApplication2.Calculator().calculation(4, 3, 4));
+        }
+        [Test]
+        public void delen2()
+        {
+            Assert.AreEqual(7892, new ConsoleApplication2.Calculator().calculation(-4, 9, 4));
+        }
+        [Test]
+        public void delen3()
+        {
+            Assert.AreEqual(1, new ConsoleApplication2.Calculator().calculation(9, 9, 4));
+        }
+    }
+}
